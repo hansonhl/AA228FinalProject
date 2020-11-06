0d6bf167ccd6c822f1f3f088a16ce1531f6173fb
Hanson Lu <hansonlu.hl@gmail.com>
2020-11-06 10:46:14 -0800

    command line input

--- main.jl
+++ main.jl
@@ -5,53 +5,66 @@ using BeliefUpdaters
 using Statistics
 using ParticleFilters
 using POMDPSimulators
-using Cairo
-using Gtk
 using Random
 using Printf
 
 include("policy_baseline.jl")
 include("policy_qmdp.jl")
 
-sensor = Lidar()
-config = 3
-m = RoombaPOMDP(sensor=sensor, mdp=RoombaMDP(config=config))
 
-num_particles = 5000
+function main()
+    config = parse(Int64, ARGS[1])
+    num_trials = parse(Int64, ARGS[2])
 
-resampler = LidarResampler(num_particles, LowVarianceResampler(num_particles))
-# for the bumper environment
-# resampler = BumperResampler(num_particles)
+    sensor = Lidar()
+    m = RoombaPOMDP(sensor=sensor, mdp=RoombaMDP(config=config))
 
-spf = BasicParticleFilter(m, resampler, num_particles)
-# spf = SIRParticleFilter(m, num_particles)
+    num_particles = 5000
 
-v_noise_coefficient = 2.0
-om_noise_coefficient = 0.5
+    resampler = LidarResampler(num_particles, LowVarianceResampler(num_particles))
+    # for the bumper environment
+    # resampler = BumperResampler(num_particles)
 
-belief_updater = RoombaParticleFilter(spf, v_noise_coefficient, om_noise_coefficient)
+    spf = BasicParticleFilter(m, resampler, num_particles)
+    # spf = SIRParticleFilter(m, num_particles)
 
-total_rewards_to_end = []
-total_rewards_qmdp = []
+    v_noise_coefficient = 2.0
+    om_noise_coefficient = 0.5
 
-save_path = "qmdp_discrete_3.jld"
-p_qmdp = load_policy(save_path)
+    belief_updater = RoombaParticleFilter(spf, v_noise_coefficient, om_noise_coefficient)
 
-for exp = 1:10
-    println(string(exp))
+    total_rewards_to_end = []
+    total_rewards_qmdp = []
 
-    Random.seed!(exp)
+    save_path = "qmdp_discrete_$(config).jld"
+    p_qmdp = load_policy(save_path)
 
-    p_to_end = ToEnd(0, get_goal_xy(m))
-    traj_rewards_to_end = sum([step.r for step in stepthrough(m,p_to_end,belief_updater, max_steps=100)])
-    traj_rewards_qmdp = sum([step.r for step in stepthrough(m,p_qmdp,belief_updater, max_steps=100)])
+    for exp = 1:10
+        println(string(exp))
 
-    push!(total_rewards_to_end, traj_rewards_to_end)
-    push!(total_rewards_qmdp, traj_rewards_qmdp)
+        Random.seed!(exp)
+
+        p_to_end = ToEnd(0, get_goal_xy(m))
+        traj_rewards_to_end = sum([step.r for step in stepthrough(m,p_to_end,belief_updater, max_steps=100)])
+        traj_rewards_qmdp = sum([step.r for step in stepthrough(m,p_qmdp,belief_updater, max_steps=100)])
+
+        push!(total_rewards_to_end, traj_rewards_to_end)
+        push!(total_rewards_qmdp, traj_rewards_qmdp)
+    end
+
+    # -1.541
+    # 2.011
+    # in 50 trials
+
+    # ToEnd Mean Total Reward: -6.004, StdErr Total Reward: 3.811
+    # QMDP Mean Total Reward: 1.054, StdErr Total Reward: 3.858
+
+    @printf("ToEnd Mean Total Reward: %.3f, StdErr Total Reward: %.3f\n", mean(total_rewards_to_end), std(total_rewards_to_end)/sqrt(5))
+    @printf("QMDP Mean Total Reward: %.3f, StdErr Total Reward: %.3f\n", mean(total_rewards_qmdp), std(total_rewards_qmdp)/sqrt(5))
+
 end
 
-# -1.541
-# 2.011
-# in 50 trials
-@printf("ToEnd Mean Total Reward: %.3f, StdErr Total Reward: %.3f\n", mean(total_rewards_to_end), std(total_rewards_to_end)/sqrt(5))
-@printf("QMDP Mean Total Reward: %.3f, StdErr Total Reward: %.3f\n", mean(total_rewards_qmdp), std(total_rewards_qmdp)/sqrt(5))
+if abspath(PROGRAM_FILE) == @__FILE__ # equivalent to if __name__ == "__main__"
+    println("Running main")
+    main()
+end
